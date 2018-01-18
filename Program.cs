599b54f482354fe850372d5e1a91ce9067abe0bf
Makhmudd <tursun_makhmut@mail.ru>
2018-01-18 19:13:43 +0600

    Second commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    public static bool IsPrime (int n)
+    {
+        for (int i = 0; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i <= args.Length; i++)
+            {
+                if (IsPrime(int.Parse(args[i])))
+                    Console.WriteLine(args[i]);
+
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
